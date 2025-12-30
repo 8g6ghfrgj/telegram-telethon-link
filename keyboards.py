d4dc98bbf27de31745b3b6f577b91e20feb75152
8g6ghfrgj <152427918+8g6ghfrgj@users.noreply.github.com>
2025-12-30 22:47:57 +0300

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,9 +0,0 @@
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-
-MAIN_MENU = InlineKeyboardMarkup([
-    [InlineKeyboardButton("👥 إدارة الحسابات", callback_data="accounts")],
-    [InlineKeyboardButton("📂 رفع روابط", callback_data="upload")],
-    [InlineKeyboardButton("🧹 تصفية الروابط", callback_data="filter")],
-    [InlineKeyboardButton("📤 توزيع الروابط", callback_data="assign")],
-    [InlineKeyboardButton("🚀 بدء الانضمام", callback_data="join")]
-])
